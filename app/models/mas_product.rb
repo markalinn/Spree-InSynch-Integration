d63e22a922b2d368c2ccd06fc99c5d977c7c892b
Mark Linn <marklinn@xwcsolutions.com>
2011-09-22 14:41:40 -0700

    New and edit screens adjusted to only display proper product listing

--- app/models/mas_product.rb
+++ app/models/mas_product.rb
@@ -1,7 +1,12 @@
 class MasProduct < ActiveRecord::Base
   set_table_name 'IM1_InventoryMasterfile'
   set_primary_key 'ItemNumber'
-  has_many :spree_product_variants,
+  has_one :spree_product_variant,
     :class_name => 'Variant',
     :foreign_key => 'sku'
+  has_one :product,
+    :through => :spree_product_variant
+    #,
+    #:joins => :product,
+    #:conditions => ['products.deleted_at is NULL']
 end
