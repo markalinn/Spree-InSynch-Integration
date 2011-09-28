6183770c6712230a8bfa4eb66fa2cd3cc882a881
Mark Linn <marklinn@xwcsolutions.com>
2011-09-28 14:56:20 -0700

    not allowing creation of products that already are deleted with same sku

--- app/models/product_decorator.rb
+++ app/models/product_decorator.rb
@@ -1,5 +1,26 @@
 Product.class_eval do
+  before_create :find_existing_deleted_sku
   after_create :populate_with_mas_data
+
+private
+  
+  def find_existing_deleted_sku
+    if self.sku
+      existing_deleted_variant = Variant.find(:first, :joins => :product, :conditions => ['products.deleted_at is not NULL and variants.sku = ?', self.sku], :readonly => false)
+      if existing_deleted_variant
+        #Don't create, instead re-enable the old deleted sku
+        dbconn = self.class.connection_pool.checkout
+        dbconn.transaction do
+          dbconn.execute("update products set deleted_at = NULL where products.id = #{existing_deleted_variant.product.id}")
+        end
+        self.class.connection_pool.checkin(dbconn)
+        #Return false so creation of new doesn't occur
+        false
+        #TODO-  Currently incorrectly returning to products index instead of /admin/products
+      end
+    end
+  end
+  
   def populate_with_mas_data
     @mas_product = MasProduct.first(:conditions => {:ItemNumber => self.sku})
     self.cost_price = @mas_product.StdCost
@@ -8,4 +29,5 @@ Product.class_eval do
     end
     self.count_on_hand = @mas_product.TotalQtyOnHand.to_i
   end
+  
 end
