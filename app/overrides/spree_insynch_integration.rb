6dc5eb79eaca7eaa72e2374fdc77dcc2a50530d5
Mark Linn <marklinn@marklinn-vm-ubuntu.(none)>
2013-03-14 13:39:32 -0700

    Various updates for variants administration

--- app/overrides/spree_insynch_integration.rb
+++ app/overrides/spree_insynch_integration.rb
@@ -3,8 +3,27 @@ Deface::Override.new(:virtual_path  => "spree/admin/products/new",
                      :partial       => "spree/admin/products/insynch_integration_new_product_attrs",
                      :name          => "admin_replace_new_product_attrs")
 
-
 Deface::Override.new(:virtual_path  => "spree/admin/products/_form",
                      :replace       => "[data-hook='admin_product_form_right']",
                      :partial       => "spree/admin/products/insynch_integration_new_product_form_right",
                      :name          => "admin_replace_new_product_form_right")
+
+Deface::Override.new(:virtual_path  => "spree/admin/variants/_form",
+                     :replace       => "[data-hook='sku']",
+                     :partial       => "spree/admin/variants/insynch_integration_variant_form_sku",
+                     :name          => "admin_replace_variant_form_sku")
+
+Deface::Override.new(:virtual_path  => "spree/admin/variants/_form",
+                     :replace       => "[data-hook='price']",
+                     :partial       => "spree/admin/variants/insynch_integration_variant_form_price",
+                     :name          => "admin_replace_variant_form_price")
+
+Deface::Override.new(:virtual_path  => "spree/admin/variants/_form",
+                     :replace       => "[data-hook='cost_price']",
+                     :partial       => "spree/admin/variants/insynch_integration_variant_form_cost_price",
+                     :name          => "admin_replace_variant_form_cost_price")
+
+Deface::Override.new(:virtual_path  => "spree/admin/variants/_form",
+                     :replace       => "[data-hook='on_hand']",
+                     :partial       => "spree/admin/variants/insynch_integration_variant_form_on_hand",
+                     :name          => "admin_replace_variant_form_on_hand")
